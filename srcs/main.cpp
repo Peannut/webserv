2ddc25e62f293b69247452fb387c797790d8ec5d
Zakaria Wina <zwina@e2r11p7.1337.ma>
2023-06-17 10:30:47 +0100

    Makefile Setup

---
+++ srcs/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+#include <ctime>
+
+#define UNUSED(x) (void)(x)
+
+int main(int argc, char ** argv)
+{
+    UNUSED(argc);
+    UNUSED(argv);
+    return 0;
+}

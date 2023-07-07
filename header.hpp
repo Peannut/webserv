0fe385955b5a53273baf76594e8f3c0206156099
SeishinU <yassinnuiga1616@gmail.com>
2023-07-07 15:28:34 +0100

    starting to figure things out

--- header.hpp
+++ header.hpp
@@ -1,5 +1,5 @@
 #ifndef HEADER_HPP
-#define HEADER_HPP
+# define HEADER_HPP
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -12,5 +12,24 @@
 #include <string.h>
 #include <stdio.h>
 #include <iostream>
+
+struct metaRequest {
+    std::string _method;
+    std::string _path;
+    std::string _version;
+    std::string _body;
+    std::string _req_raw;
+    std::vector<std::pair<std::string, std::string>> _headers;
+};
+
+struct metaResponse {
+    int statusCode;
+    std::string statusMessage;
+    std::string contentType;
+    std::string content;
+
+    metaResponse(int code, const std::string& message, const std::string& type, const std::string& body)
+        : statusCode(code), statusMessage(message), contentType(type), content(body) {}
+};
 
 #endif
